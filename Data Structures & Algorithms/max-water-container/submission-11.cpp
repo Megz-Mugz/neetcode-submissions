3f34db0146287f0540d2b19636e523b90ef2cda4
MegzMugz <69176595+Megz-Mugz@users.noreply.github.com>
2026-04-12 16:24:33 -0400

    Add: max-water-container - submission-11

---
+++ Data Structures & Algorithms/max-water-container/submission-11.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int left = 0, right = heights.size() - 1; 
+        size_t max_area = 0;
+
+        while (left < right){
+            size_t max_height = std::min(heights[left], heights[right]);
+            size_t length = right - left;
+
+            max_area = std::max(max_area, max_height * length);
+
+            if (heights[left] < heights[right]){
+                left++;
+            } else {
+                right--;
+            }
+
+        }
+
+        return max_area;
+    }
+};
