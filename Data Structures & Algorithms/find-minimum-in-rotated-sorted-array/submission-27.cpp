d076eab6c7786a1722cc9a1395e5d82ce5a8a824
MegzMugz <69176595+Megz-Mugz@users.noreply.github.com>
2026-06-25 12:56:38 -0400

    Add: find-minimum-in-rotated-sorted-array - submission-27

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-27.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int findMin(vector<int> &nums) {
+        // O(log n) -> instead of linear search, what about binary seaerch?
+
+        /*
+            [3,4,5,6,1,2]
+            left = 0
+            right = 5 
+
+            middle = 2
+        
+            if nums[left] < nums[right] -> return nums[left]
+
+            else
+                check if middle is the min number
+
+                if nums[middle] > nums[left] -> min number is not here, left = middle + 1
+                else -> min number is between left and middle, inclusive, right = middle - 1
+        */
+
+        int left = 0, right = nums.size() - 1;
+        int min_number = nums[0];
+
+        while (left <= right){
+
+            int middle = left + (right - left) / 2;
+            
+            if (nums[left] <= nums[right]){
+                
+                break;
+
+            } else {
+                // check if the middle number is the min number
+                min_number = std::min(min_number, nums[middle]);
+
+                if (nums[middle] >= nums[left]){
+                    left = middle + 1;
+                } else {
+                    right = middle - 1;
+                }
+            }
+        }
+
+        return std::min(min_number, nums[left]);
+    }
+};
