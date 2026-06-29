8f865b906bd35f4ba341835501fcb80f03319970
MegzMugz <69176595+Megz-Mugz@users.noreply.github.com>
2026-06-29 10:02:24 -0400

    Add: time-based-key-value-store - submission-18

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-18.cpp
@@ -0,0 +1,40 @@
+class TimeMap {
+private:
+    using key = string;
+    using value = string;
+    using timestamp = int;
+
+    std::unordered_map<key, std::vector<std::pair<value, timestamp>>> storage;
+
+public:
+    TimeMap() {}
+    
+    void set(string key, string value, int timestamp) {
+        // use emplace_back since it constructs a pair for you
+        storage[key].emplace_back(value, timestamp);
+    }
+    
+    string get(string key, int timestamp) {
+        // Guard rail - if key doesn't have any set values, return an empty string
+        if (storage[key].size() == 0) return "";
+
+        int left = 0;
+        int right = storage[key].size() - 1;
+        string most_recent_key = "";
+
+        while (left <= right){
+            int middle = left + (right - left) / 2;
+            auto current_key = storage[key];
+
+            // ensure that timestamp at middle <= timestamp
+            if (current_key[middle].second > timestamp){
+                right = middle - 1;
+            } else {
+                most_recent_key = current_key[middle].first;
+                left = middle + 1;
+            }
+        }
+
+        return most_recent_key;
+    }
+};
