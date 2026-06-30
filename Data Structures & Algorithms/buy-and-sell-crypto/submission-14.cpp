a09770a4f4b217c865a8e7827b33852169336b74
MegzMugz <69176595+Megz-Mugz@users.noreply.github.com>
2026-06-30 09:53:08 -0400

    Add: buy-and-sell-crypto - submission-14

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-14.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int max_profit = 0;
+        int buy_ptr = 0, sell_ptr = 1;
+
+        while (sell_ptr < prices.size()){
+            // ensure the buying price is less than the selling price
+            if (prices[sell_ptr] < prices[buy_ptr]){
+                buy_ptr = sell_ptr;
+                sell_ptr++;
+                continue;
+            }
+            // compute profit
+            max_profit = std::max(max_profit, prices[sell_ptr] - prices[buy_ptr]);
+            sell_ptr++;
+        }
+
+        return max_profit;
+    }
+};
